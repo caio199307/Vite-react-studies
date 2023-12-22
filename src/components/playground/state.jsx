cc230244dc042e57cdabbad63a8e260ef5e6ff1d
Caio <caio199307@gmail.com>
2023-12-22 04:29:40 -0300

    use state

--- src/components/playground/state.jsx
+++ src/components/playground/state.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react"
 
 const StatePlayground = () => {
+    let initialCount = 0
 
-    let [count, setCount] = useState(0);
+    let [count, setCount] = useState(initialCount);
 
     const addOne = () => {
         setCount(count+1)
     }
+
+    const restOne = () => {
+        setCount(prevCount => prevCount - 1)
+    }
+
+    const resetCount = () => {
+        setCount(initialCount)
+    }
+
     return (
         <>
             <h3>Count: {count}</h3>
             <button onClick={addOne}>Add one +1</button>
+            <button onClick={restOne}>rest one -1</button>
+            <button onClick={resetCount}>RESET</button>
         </>
     )
 }
